1c2276be82f394b0f1875c590ffcc60558358a23
qudlaty <106681377+Chriss350@users.noreply.github.com>
2022-12-08 18:39:50 +0100

    02 fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,24 +4,14 @@ const iframe = document.querySelector('iframe');
 const throttle = require('lodash.throttle');
 const player = new Vimeo(iframe);
 
-player.getDuration();
-
 player.on(
   'timeupdate',
   throttle(e => {
-    player.getDuration().then(function (duration) {
-      let dur = duration.toFixed(0);
-      let curr = (time = e.seconds.toFixed(0));
-      if (dur === curr) {
+    player.getEnded().then(function (ended) {
+      if (ended === true) {
         return localStorage.removeItem('pct');
       }
     });
-
-    // player.getEnded().then(function (ended) {
-    //   if (ended === true) {
-    //     return localStorage.removeItem('pct');
-    //   }
-    // });
 
     localStorage.setItem('pct', e.seconds);
   }, 1000)
